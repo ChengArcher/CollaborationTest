11fea77d28e3e21a000b7bf3e7e35da9520cf10b
arrrrcher <arrrrcher@archerrrrrr.com>
2024-08-03 16:05:29 +0800

    AAAAAAAAAAAAA

--- Calculator.java
+++ Calculator.java
@@ -12,7 +12,7 @@ public class Calculator {
         return a * b;
     }
 
-    public int Archerrrrr(int a, int b) {
+    public int Archeraaaaaaarrrr(int a, int b) {
         return a * b;
     }
 }
